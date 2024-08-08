af812d7aedcbf918174f476e35cfd4d89a971362
dev sharma <devsharma45637@gmail.com>
2024-08-08 03:00:42 -0800

    msg

--- routes/user.js
+++ routes/user.js
@@ -1,34 +1,55 @@
+
 const express = require("express");
 const router = express.Router();
-const user = require("../models/user.js");
-const wrapAsync = require("../utils/wrapAsync");
-const passport=require("passport");
+const User = require("../models/user.js");
+const wrapasync = require("../utils/wrapasync.js");
+const passport = require("passport");
+const { saveRedirecturl } = require("../middleware.js");
 
-router.get("/signup",(req,res)=>{
+router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
-});
+})
 
-router.post("/signup", wrapAsync(async(req,res)=>{
-    console.log("hello");
-    try{
-        const {username,Email,password} = req.body;
-        console.log(req.body);
-     const newUser = new User({email:Email,username});
-       const registerUser = await User.register(newUser ,password)
-       req.flash("success","Welcome to wonderLust");
-       res.redirect("/listings");
-    } catch(e){
-        req.flash("error",e.message);
+router.post("/signup", wrapasync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({
+            email, username
+        });
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to Wanderlust");
+            res.redirect("/listings");
+        });
+    } catch (e) {
+        req.flash("error", e.message);
         res.redirect("/signup");
     }
 }));
 
-router.get("/login" , (req,res)=>{
+
+router.get("/login", (req, res) => {
     res.render("users/login.ejs");
+})
+
+router.post("/login", passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), async (req, res) => {
+    req.flash("success", "Welcome to Wanderlust! You are logged in!")
+    res.redirect("/listings");
+
 });
 
-router.post("/login",passport.authenticate(),async(req , res)=>{
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "you are logged out now");
+        res.redirect("/listings");
+    })
+})
 
-} )
 module.exports = router;
-
